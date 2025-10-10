19ac068bec955d9a4db7413afe716cb35d51208d
Shankha Nag <156122218+nagshankha@users.noreply.github.com>
2025-10-10 22:19:22 +0000

    created the basic structure... pyiron_workflow wrapping part is remaining

--- src/ovito2pyiron.py
+++ src/ovito2pyiron.py
@@ -1,4 +1,5 @@
 import ovito
+from ovito.modifiers import PythonModifier
 
 class Ovito2Pyiron:
     def __init__(self, ovito_state_filepath):
@@ -13,4 +14,26 @@ class Ovito2Pyiron:
         return self._imported_pipeline
 
     def _create_pyiron_workflow(self):
-        for i in len(self.imported_pipeline.modifiers)
+
+        for i in len(self.imported_pipeline.modifiers):
+            mod = self.imported_pipeline.modifiers[i]
+
+            if isinstance(mod, ovito.modifiers.PythonModifier):
+                # User-defined PythonModifier
+                args_dict = vars(mod.delegate)
+                cls = type(mod.delegate)
+                new_delegate = cls(**args_dict)
+
+                new_mod = PythonModifier(function=new_delegate.modify)
+                new_mod.delegate = new_delegate
+                new_pipeline.modifiers.append(new_mod)
+
+            else:
+                # Built-in modifier
+                cls = type(mod)
+                arg_names = [
+                    name for name, attr in vars(cls).items()
+                    if type(attr) is property
+                ]
+                args_dict = {name: getattr(mod, name) for name in arg_names}
+                new_pipeline.modifiers.append(cls(**args_dict))
